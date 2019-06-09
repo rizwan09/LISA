9e6186190726be5f381c622b87b8e809bb98d2cf
Md Rizwan Parvez <rizwan@nlp9.cs.ucla.edu>
2019-06-09 06:46:04 -0700

    before changing to two conv

--- SAST/src/vocab_syntax_BERT.py
+++
@@ -1,8 +0,0 @@
-import os
-run_command = 'python /home/rizwan/SBCR/tensor2tensor/tensor2tensor/data_generators/text_encoder_build_subword.py \
-    --corpus_filepattern=/home/rizwan/SBCR/SAST/SyntaxCorpus/vocab/dummy.txt\
-    --corpus_max_lines=1000000000000000\
-    --output_filename=/home/rizwan/SBCR/SAST/SyntaxCorpus/vocab/log.txt \
-    --logtostderr'
-
-os.system(run_command)
